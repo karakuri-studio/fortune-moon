2f1a52e0943af10437db9b12240b50b50460718f
kenji1031-dev <kenji1031@gmail.com>
2026-02-23 00:56:58 +0900

    add build web

---
+++ android/app/src/main/kotlin/com/example/fortune_moon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fortune_moon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- fortune_moon/android/app/src/main/kotlin/com/example/fortune_moon/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fortune_moon
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
